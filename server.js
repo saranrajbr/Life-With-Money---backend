b18fadbfbaaebaaf5bbb4cc174b2d9e8127e21b1
Saran Raj <saranrajbr63@gmail.com>
2026-02-12 00:00:37 +0530

    .

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ cursor();
 
 const app=express();
 app.use(cors({
-    origin:'saranrajbr.github.io/Life-With-Money/',
+    origin:['https://saranrajbr.github.io', 'http://localhost:5173'],
     credentials:true,
     methods:['GET','POST','PUT','DELETE','OPTIONS'],
     allowedHeaders: ['Content-Type','Authorization']
